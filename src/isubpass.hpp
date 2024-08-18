8c93659380c952e350548ee99fe6704d1bd1495f
SarpAkin <sarp4.akin@gmail.com>
2024-08-18 14:00:44 +0300

    added constructor to SubpassDescription

--- src/isubpass.hpp
+++ src/isubpass.hpp
@@ -7,13 +7,16 @@ namespace vke {
 
 class ISubpass {
 public:
-    virtual u32 get_subpass_index()const        = 0;
-    virtual VkRenderPass get_renderpass()const = 0;
-    virtual u32 get_attachment_count()const    = 0;
+    virtual u32 get_subpass_index() const       = 0;
+    virtual VkRenderPass get_renderpass() const = 0;
+    virtual u32 get_attachment_count() const    = 0;
 };
 
 class SubpassDescription : public ISubpass {
 public:
+    SubpassDescription(VkRenderPass renderpass,u32 subpass_index, u32 attachment_count)
+        : subpass_index(subpass_index), renderpass(renderpass), attachment_count(attachment_count) {}
+
     u32 subpass_index, attachment_count;
     VkRenderPass renderpass;
 
