01a5e3941af21bcd9bf6f8e27e481ced3c7b39e3
SarpAkin <sarp4.akin@gmail.com>
2024-08-29 21:30:44 +0300

    new resource mode external

--- src/vk_resource.cpp
+++ src/vk_resource.cpp
@@ -11,18 +11,26 @@ VkDevice DeviceGetter::device() const {
 
 VulkanContext* DeviceGetter::get_context() { return VulkanContext::get_context(); }
 
-
 Resource::Resource() {}
 Resource::~Resource() {}
 
 RCResource<Resource> Resource::get_reference() {
-    if(!m_is_reference_counted){
-        assert(!"resource must be reference counted");
-        abort();
-    }
-    
-    increment_reference_count();
+    auto ref = try_get_reference();
+
+    assert(ref.get() != nullptr);
 
     return RCResource(this);
+}
+
+RCResource<Resource> Resource::try_get_reference() {
+    switch (m_ownership) {
+    case OwnerShip::OWNED:
+        return RCResource<Resource>();
+    case OwnerShip::RefCounted:
+        increment_reference_count();
+        return RCResource(this);
+    case OwnerShip::EXTERNAL:
+        return m_external->try_get_reference();
+    }
 }
 } // namespace vke
