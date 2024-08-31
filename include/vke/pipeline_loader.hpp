b0c89c13bf9ad96867caa3f618fb637e3064a0a3
SarpAkin <sarp4.akin@gmail.com>
2024-08-31 22:43:07 +0300

    added pipeline loader to includes

---
+++ include/vke/pipeline_loader.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "../src/pipeline/loader/debug_loader.hpp"
+#include "../src/pipeline/loader/ipipeline_loader.hpp"
+#include "../src/pipeline/loader/pipeline_globals_provider.hpp"
+
