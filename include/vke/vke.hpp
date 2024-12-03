37504e9e96ea8cad458649eb9a5f41482e82a728
SarpAkin <sarp4.akin@gmail.com>
2024-12-03 11:43:31 +0300

    updated the includes

--- include/vke/vke.hpp
+++ include/vke/vke.hpp
@@ -2,16 +2,18 @@
 
 #include "fwd.hpp" // IWYU pragma: export
 
-#include "../../src/buffer.hpp"          // IWYU pragma: export
-#include "../../src/commandbuffer.hpp"   // IWYU pragma: export
-#include "../../src/common.hpp"          // IWYU pragma: export
-#include "../../src/descriptor_pool.hpp" // IWYU pragma: export
-#include "../../src/fwd.hpp"             // IWYU pragma: export
-#include "../../src/isubpass.hpp"        // IWYU pragma: export
-#include "../../src/pipeline.hpp"        // IWYU pragma: export
-#include "../../src/vulkan_context.hpp"  // IWYU pragma: export
-#include "../../src/fence.hpp"  // IWYU pragma: export
-#include "../../src/semaphore.hpp"  // IWYU pragma: export
-#include "../../src/image.hpp"  // IWYU pragma: export
-#include "../../src/image_view.hpp"  // IWYU pragma: export
-#include "../../src/sparse_resources/growable_buffer.hpp"  // IWYU pragma: export
+#include "../../src/buffer.hpp"                           // IWYU pragma: export
+#include "../../src/commandbuffer.hpp"                    // IWYU pragma: export
+#include "../../src/common.hpp"                           // IWYU pragma: export
+#include "../../src/descriptor_pool.hpp"                  // IWYU pragma: export
+#include "../../src/fence.hpp"                            // IWYU pragma: export
+#include "../../src/fwd.hpp"                              // IWYU pragma: export
+#include "../../src/image.hpp"                            // IWYU pragma: export
+#include "../../src/image_view.hpp"                       // IWYU pragma: export
+#include "../../src/isubpass.hpp"                         // IWYU pragma: export
+#include "../../src/pipeline.hpp"                         // IWYU pragma: export
+#include "../../src/semaphore.hpp"                        // IWYU pragma: export
+#include "../../src/sparse_resources/growable_buffer.hpp" // IWYU pragma: export
+#include "../../src/vulkan_context.hpp"                   // IWYU pragma: export
+#include "../../src/window/surface.hpp"                   // IWYU pragma: export
+#include "../../src/window/window.hpp"                    // IWYU pragma: export
