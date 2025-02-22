382456fff6226c51fa32cc8fb2c2b8b11215f30d
SarpAkin <sarp4.akin@gmail.com>
2025-02-22 11:17:40 +0300

    added ID manager

--- include/vke/util.hpp
+++ include/vke/util.hpp
@@ -3,3 +3,4 @@
 #include "../src/util/util.hpp" // IWYU pragma: export
 #include "../src/vkutil.hpp"    // IWYU pragma: export
 #include "../src/util/slim_vec.hpp"    // IWYU pragma: export
+#include "../src/util/id_manager.hpp"    // IWYU pragma: export
