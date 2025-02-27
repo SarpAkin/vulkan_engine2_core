3edda71e60bd88320817b693262ae5fa4b127d3e
SarpAkin <sarp4.akin@gmail.com>
2025-02-27 12:46:09 +0300

    added the utility class VirtualAllocator which is abstraction over VmaVirtualAllocator

---
+++ src/util/virtual_allocator.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "vke/fwd.hpp"
+#include <optional>
+
+typedef struct VmaVirtualAllocation_T* VmaVirtualAllocation;
+typedef struct VmaVirtualBlock_T* VmaVirtualBlock;
+
+namespace vke {
+
+// a wrapper around VmaVirtualAllocator
+class VirtualAllocator {
+public:
+    struct Allocation {
+        VmaVirtualAllocation allocation = nullptr;
+        u32 offset                      = 0;
+        u32 size                        = 0;
+    };
+
+public:
+    std::optional<Allocation> allocate(u32 size, usize alignment = 1);
+    void free(Allocation allocation);
+
+    VmaVirtualBlock get_virtual_block() { return m_virtual_block; }
+
+public:
+    VirtualAllocator(u32 block_size);
+    ~VirtualAllocator();
+
+    VirtualAllocator(const VirtualAllocator& other)            = delete;
+    VirtualAllocator& operator=(const VirtualAllocator& other) = delete;
+
+    VirtualAllocator(VirtualAllocator&& other) {
+        m_virtual_block       = other.m_virtual_block;
+        other.m_virtual_block = nullptr;
+    }
+
+    VirtualAllocator& operator=(VirtualAllocator&& other) {
+        m_virtual_block       = other.m_virtual_block;
+        other.m_virtual_block = nullptr;
+        return *this;
+    }
+
+private:
+    VmaVirtualBlock m_virtual_block = nullptr;
+};
+
+}; // namespace vke
