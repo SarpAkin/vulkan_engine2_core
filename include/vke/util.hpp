23cce39669b5c9b9b44f961e20e2fdbef2ab9776
SarpAkin <sarp4.akin@gmail.com>
2025-03-03 17:24:41 +0300

    added virtual_allocator to includes

--- include/vke/util.hpp
+++ include/vke/util.hpp
@@ -4,5 +4,5 @@
 #include "../src/util/slim_vec.hpp"          // IWYU pragma: export
 #include "../src/util/stencil_buffer.hpp"    // IWYU pragma: export
 #include "../src/util/util.hpp"              // IWYU pragma: export
+#include "../src/util/virtual_allocator.hpp" // IWYU pragma: export
 #include "../src/vkutil.hpp"                 // IWYU pragma: export
-
